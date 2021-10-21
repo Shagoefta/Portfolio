47da42e0c5308ce67d1e71272e28eb18af057182
Shagoefta <91876056+Shagoefta@users.noreply.github.com>
2021-10-21 15:23:47 +0200

    Delete Opdracht 8-1.py

--- Opdracht 8-1.py
+++
@@ -1,9 +0,0 @@
-thisdict = {
-    "First name:": "Coen",
-    "Last name:": "Meulenkamp",
-    "Job title:": "Learning Coach",
-    "Company:": "Techgrounds"
-    }
-
-for x, y in thisdict.items():
-    print(x,y)
